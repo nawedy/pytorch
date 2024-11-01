c931322b47942ebda9bf8582ea55a7115fc06209
Yidi Wu <yidi@meta.com>
2024-11-01 16:09:19 -0700

    [hop][lift free symbols] lift symbols in subgraph recursively
    ghstack-source-id: a4375feb2b0a8880021457dd4cf7ef9f94a582e5
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/138363

--- torch/_dynamo/variables/tensor.py
+++ torch/_dynamo/variables/tensor.py
@@ -758,6 +758,7 @@ class TensorVariable(VariableTracker):
 
     def method_tolist(self):
         from ..symbolic_convert import InstructionTranslator
+        from .builder import wrap_fx_proxy
 
         tx = InstructionTranslator.current_tx()
 
@@ -768,10 +769,7 @@ class TensorVariable(VariableTracker):
                 with unittest.mock.patch.object(
                     tx.fake_mode, "allow_scalar_outputs", True
                 ):
-                    return SymNodeVariable.create(
-                        tx,
-                        sub_proxy.item(),
-                    )
+                    return wrap_fx_proxy(tx, sub_proxy.item())
 
             if tensor.dtype not in [
                 torch.int8,
